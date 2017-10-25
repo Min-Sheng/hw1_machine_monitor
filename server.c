55224f1c3eb19b6a2c087693d6b15beeadfffcca
F94036089 <f94036089@ncku.edu.tw>
2017-10-25 10:47:26 +0800

    Build the server socket.

--- server.c
+++ server.c
@@ -1,7 +1,12 @@
 #include "server.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <netinet/in.h>
 //void *communicate_to_client();
 void a_all_pids();
 void b_tids(int pid);
@@ -15,11 +20,91 @@ void i_Vmsize(int pid);
 void j_VmRSS(int pid);
 void k_exit();
 
+void socket_server();
+
 int main(int argc, char **argv)
 {
 // write someting here...
-	a_all_pids();
+	socket_server();
+	//a_all_pids();
+	//printf("%d\n", SERVER_PORT);
+	//printf("%d\n", BUFFER_SIZE);
 	return 0;
+}
+
+void socket_server()
+{
+
+	struct sockaddr_in server_addr;
+	int connection_socket;
+	int ret;
+	char buffer[BUFFER_SIZE] = "Hi client! ";
+	char buffer1[BUFFER_SIZE];
+	int reuseaddr = 1;
+	socklen_t reuseaddr_len;
+
+	/* create sockett */
+	connection_socket = socket(PF_INET, SOCK_STREAM, 0);
+	if (connection_socket == -1) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	/* initialize structure server_addr */
+	memset(&server_addr, 0, sizeof(struct sockaddr_in));
+	//bzero(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERVER_PORT);
+	/* this line is different from client */
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	reuseaddr_len = sizeof(reuseaddr);
+	setsockopt(connection_socket, SOL_SOCKET, SO_REUSEADDR, &reuseaddr,
+	           reuseaddr_len);
+
+	/* Assign a port number to socket */
+	ret = bind(connection_socket, (struct sockaddr*)&server_addr,
+	           sizeof(struct sockaddr_in));
+
+	if (ret == -1) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	/* make it listen to socket with max 20 connections */
+	ret = listen(connection_socket, 20);
+	if (ret == -1) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	/* infinity loop -- accepting connection from client forever */
+	while(1) {
+		int client_socket;
+		struct sockaddr_in client_addr;
+		socklen_t addrlen = sizeof(client_addr);
+
+		/* Wait and Accept connection */
+		client_socket = accept(connection_socket, (struct sockaddr*)&client_addr,
+		                       (socklen_t *)&addrlen);
+		if (client_socket == -1) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+
+		/* Send message */
+		send(client_socket, buffer, sizeof(buffer), 0);
+		int res=recv(client_socket, buffer1, sizeof(buffer1), 0);
+		printf("receive from client: %s, %d bytes\n", buffer1,res);
+		//write(fd, buffer1, sizeof(buffer1));
+		/* close(client) */
+		close(client_socket);
+
+	}
+
+	/* close(server) , but never get here because of the loop */
+	close(connection_socket);
+
 }
 
 void a_all_pids()
