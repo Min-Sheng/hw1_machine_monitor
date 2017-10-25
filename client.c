deae30308d8c2b9fb78778cb9071406494ae91b0
F94036089 <f94036089@ncku.edu.tw>
2017-10-25 17:29:33 +0800

    Add (b)

--- client.c
+++ client.c
@@ -8,14 +8,18 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+void print_ask();
+
 int main(int argc, char **argv)
 {
 // write someting here...
 	struct sockaddr_in client_addr;
 	int client_socket;
 	int ret;
-	char buffer[BUFFER_SIZE];
-	char resp[10]="clientack";
+	char quest[1]="";
+	char ans[BUFFER_SIZE];
+	//char buffer[BUFFER_SIZE];
+	//char resp[10]="clientack";
 
 	/* create socket */
 	client_socket = socket(PF_INET, SOCK_STREAM, 0);
@@ -31,25 +35,68 @@ int main(int argc, char **argv)
 	client_addr.sin_port = htons(SERVER_PORT);
 	client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 //inet_aton("127.0.0.1", &client_addr.sin_addr);
+	printf("Linking to server...");
 
-	/* Connecting to server */
-	ret = connect(client_socket, (struct sockaddr*)&client_addr,
-	              sizeof(struct sockaddr_in));
+	while(1) {
 
-	if (ret == -1) {
-		fprintf(stderr, "The server is down.\n");
-		exit(EXIT_FAILURE);
+		printf("\n====================================================\n");
+
+		/* Connecting to server */
+		ret = connect(client_socket, (struct sockaddr*)&client_addr,
+		              sizeof(struct sockaddr_in));
+
+		if (ret == -1) {
+			fprintf(stderr, "The server is down.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		else {
+			print_ask();
+		}
+
+		/*Send quest*/
+		memset(&quest, 0, 1);
+		if(scanf("%c",&quest[0]) == 1) {
+			if (quest[0] == 'a') {
+				send(client_socket,quest,sizeof(quest),0);
+				printf("\n");
+			} else if (quest[0] == 'b') {
+				send(client_socket,quest,sizeof(quest),0);
+				printf("\n");
+				printf("[tid] ");
+			}
+		} else {
+			printf("Failed.\n");
+		}
+		/* Receive ans from the server and print to screen */
+		memset(&ans, 0, BUFFER_SIZE);
+
+		while(1) {
+			int res = recv(client_socket, ans, sizeof(ans), 0);
+			if(res == 0) {
+				break;
+			}
+
+			printf("%s\n", ans);
+		}
 	}
-
-	/* Receive message from the server and print to screen */
-	memset(buffer, 0, BUFFER_SIZE);
-	recv(client_socket, buffer, sizeof(buffer), 0);
-	printf("receive from server: %s\n", buffer);
-	send(client_socket,resp,sizeof(resp),0);
-
 	/* Close connection */
 	close(client_socket);
 
 	return 0;
 }
-
+void print_ask()
+{
+	printf("(a) list all process ids\n");
+	printf("(b) thread's IDs\n");
+	printf("(c) clild's PIDs\n");
+	printf("(d) process name\n");
+	printf("(e) state of process (D,R,S,T,t,W,X,Z)\n");
+	printf("(f) command line of excuting process (cmdline)\n");
+	printf("(g) parent's PID\n");
+	printf("(h) all ancients of PIDs\n");
+	printf("(i) virtual memory size (Vmsize)\n");
+	printf("(j) physical memory size (VmRSS)\n");
+	printf("(k) exit\n");
+	printf("which? ");
+}
