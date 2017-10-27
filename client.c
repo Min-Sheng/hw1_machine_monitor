f5fd879cef0f04af6644d771906aff1f5f017530
F94036089 <f94036089@ncku.edu.tw>
2017-10-27 21:37:47 +0800

    Finish the (a,b,d,e,g,i,j,k)

--- client.c
+++ client.c
@@ -8,8 +8,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-void print_ask();
-
 int main(int argc, char **argv)
 {
 	struct sockaddr_in client_addr;
@@ -32,29 +30,37 @@ int main(int argc, char **argv)
 	client_addr.sin_port = htons(SERVER_PORT);
 	client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	printf("Linking to server...");
+	printf("Linking to server...\n");
+	/* Connecting to server */
+	ret = connect(client_socket, (struct sockaddr*)&client_addr,
+	              sizeof(struct sockaddr_in));
+	if (ret == -1) {
+		fprintf(stderr, "The server is down.\n");
+		exit(EXIT_FAILURE);
+	}
 
 	while(1) {
 
-		printf("\n====================================================\n");
+		printf("====================================================\n");
 
-		/* Connecting to server */
-		ret = connect(client_socket, (struct sockaddr*)&client_addr,
-		              sizeof(struct sockaddr_in));
+		printf("(a) list all process ids\n");
+		printf("(b) thread's IDs\n");
+		printf("(c) clild's PIDs\n");
+		printf("(d) process name\n");
+		printf("(e) state of process (D,R,S,T,t,W,X,Z)\n");
+		printf("(f) command line of excuting process (cmdline)\n");
+		printf("(g) parent's PID\n");
+		printf("(h) all ancients of PIDs\n");
+		printf("(i) virtual memory size (Vmsize)\n");
+		printf("(j) physical memory size (VmRSS)\n");
+		printf("(k) exit\n");
+		printf("which? ");
 
-		if (ret == -1) {
-			fprintf(stderr, "The server is down.\n");
-			exit(EXIT_FAILURE);
-		}
-
-		else {
-			print_ask();
-		}
-
+		setbuf(stdin,NULL);
 		/*Send quest*/
 		memset(&quest, 0, 1);
 		memset(&pid, 0, 1);
-		if(scanf("%c",&quest[0]) == 1) {
+		if(scanf("%s",quest) == 1) {
 			if (quest[0] == 'a') {
 				send(client_socket,quest,sizeof(quest),0);
 				printf("\n");
@@ -68,6 +74,52 @@ int main(int argc, char **argv)
 				} else {
 					printf("Failed.\n");
 				}
+			} else if(quest[0] == 'd') {
+				send(client_socket, quest, sizeof(quest), 0);
+				printf("pid? ");
+				if(scanf("%d",&pid[0]) == 1) {
+					send(client_socket, pid, sizeof(pid), 0);
+					printf("\n");
+					printf("[Name] ");
+				}
+			} else if(quest[0] == 'e') {
+				send(client_socket, quest, sizeof(quest), 0);
+				printf("pid? ");
+				if(scanf("%d",&pid[0]) == 1) {
+					send(client_socket, pid, sizeof(pid), 0);
+					printf("\n");
+					printf("[State] ");
+				}
+			} else if(quest[0] == 'g') {
+				send(client_socket, quest, sizeof(quest), 0);
+				printf("pid? ");
+				if(scanf("%d",&pid[0]) == 1) {
+					send(client_socket, pid, sizeof(pid), 0);
+					printf("\n");
+					printf("[ppid] ");
+				}
+			} else if(quest[0] == 'i') {
+				send(client_socket, quest, sizeof(quest), 0);
+				printf("pid? ");
+				if(scanf("%d",&pid[0]) == 1) {
+					send(client_socket, pid, sizeof(pid), 0);
+					printf("\n");
+					printf("[Vmsize] ");
+				}
+			} else if(quest[0] == 'j') {
+				send(client_socket, quest, sizeof(quest), 0);
+				printf("pid? ");
+				if(scanf("%d",&pid[0]) == 1) {
+					send(client_socket, pid, sizeof(pid), 0);
+					printf("\n");
+					printf("[VmRSS] ");
+				}
+			} else if(quest[0] == 'k') {
+				send(client_socket, quest, sizeof(quest), 0);
+				printf("The client exited.\n");
+				break;
+			} else {
+				printf("Please enter the correct character.\n");
 			}
 		} else {
 			printf("Failed.\n");
@@ -76,8 +128,8 @@ int main(int argc, char **argv)
 		memset(&ans, 0, BUFFER_SIZE);
 
 		while(1) {
-			int res = recv(client_socket, ans, sizeof(ans), 0);
-			if(res == 0) {
+			recv(client_socket, ans, sizeof(ans), 0);
+			if (ans[0]=='\0') {
 				break;
 			}
 			printf("%s\n", ans);
@@ -87,19 +139,4 @@ int main(int argc, char **argv)
 	close(client_socket);
 
 	return 0;
-}
-void print_ask()
-{
-	printf("(a) list all process ids\n");
-	printf("(b) thread's IDs\n");
-	printf("(c) clild's PIDs\n");
-	printf("(d) process name\n");
-	printf("(e) state of process (D,R,S,T,t,W,X,Z)\n");
-	printf("(f) command line of excuting process (cmdline)\n");
-	printf("(g) parent's PID\n");
-	printf("(h) all ancients of PIDs\n");
-	printf("(i) virtual memory size (Vmsize)\n");
-	printf("(j) physical memory size (VmRSS)\n");
-	printf("(k) exit\n");
-	printf("which? ");
 }
