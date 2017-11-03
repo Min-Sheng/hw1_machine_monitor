aba3c8a7b1d52c5db91ea0f60e6d12524d019ff5
F94036089 <f94036089@ncku.edu.tw>
2017-11-04 00:18:52 +0800

    Add pthread & revise the socket with getaddrinfo()

--- protocol.h
+++ protocol.h
@@ -1,10 +1,17 @@
+/*************************************************
+		Class  : OS
+		Project: Hw1_machine_monitor
+		Author : Min-Sheng Wu
+*************************************************/
 #ifndef HW1_PROTCL_H
 #define HW1_PROTCL_H
 
 struct monitor_protocol {
 // write someting here...
-#define SERVER_PORT 59487
+#define SERVER_PORT "59487"
 #define BUFFER_SIZE 256
+#define MAX_CLIENT 20
+#define BACKLOG 20
 };
 
 #endif
